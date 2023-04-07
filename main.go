6d1c376fe4ea007f4613fd01acde3b0467256a57
MetehanSarii <smetehan5252@gmail.com>
2023-04-07 20:14:55 +0300

    Loops2

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+/*Deneme
+package main
+
+import "fmt"
+
+func Demo3() {
+	var sayi1, sayi2, sayi3 int = 100, 70, 90
+	var enBuyuk int = sayi1
+
+	if sayi2 > enBuyuk {
+		enBuyuk = sayi2
+	}
+	if sayi3 > enBuyuk {
+		enBuyuk = sayi3
+	}
+	fmt.Printf("En Büyüük Sayı: %v", enBuyuk)
+}
+func main() {
+	Demo3()
+}*/
+
+// else if
+/*package main
+
+import "fmt"
+
+func Demo1() {
+	var hesap float64 = 1000
+	var cekilmekistenen float64 = 500
+	if cekilmekistenen > hesap {
+		fmt.Println("Yetersiz Bakiye")
+	}else {[
+	]}
+*/
+// if
+/*package main
+
+import "fmt"
+
+func Demo1() {
+	var hesap float64 = 1000
+	var cekilmekistenen float64 = 500
+	if cekilmekistenen > hesap {
+		fmt.Println("Yetersiz Bakiye")
+	}
+	if cekilmekistenen <= hesap {
+		fmt.Println("İşlem Başarılı")
+		hesap = hesap - cekilmekistenen
+	}
+	fmt.Print("BİTTİ - hesapta kalan para:  "+fmt.Sprintf("%v"), hesap)
+}
+func main() {
+	Demo1()
+}*/
